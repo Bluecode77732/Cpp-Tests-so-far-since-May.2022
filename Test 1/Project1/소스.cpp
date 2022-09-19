8c9ccce8b4cdb61c9c36b9bb8ddf09e29606db6a
Bluecode77732 <63056375+Bluecode77732@users.noreply.github.com>
2022-09-19 19:12:42 +0900

    Add files via upload

---
+++ Test 1/Project1/소스.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+
+	class libertarian {
+	public:
+		int a;
+	private:
+		int : 3;
+	};
+
+	int main() {
+		MyClass myObj;  // Create an object of MyClass
+
+	// Access attributes and set values
+	myObj.myNum = 15;
+	myObj.myString = "Some text";
+
+	// Print attribute values
+	cout << myObj.myNum << "\n";
+	cout << myObj.myString;
+	return 0;
+	}
+
+
+}
